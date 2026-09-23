1496fbd65c61095540c772006b119efadc9f225b
Eprashala <58760965+Eprashala@users.noreply.github.com>
2026-09-23 11:55:48 +0530

    Update appteach1.js

--- appteach1.js
+++ appteach1.js
@@ -45,7 +45,7 @@ function enforceFullscreen() {
 
 
 // --- 1. DATA STRUCTURES & CONFIG ---
-const PROXY_BASE_URL = "https://eprashala-proxy-511804777001.asia-south1.run.app";
+const PROXY_BASE_URL = "https://eprashala.pythonanywhere.com";
 
 async function fetchGeminiChat(payloadObject, abortSignal, modelId) {
     const userKey = document.getElementById('custom-api-key-input').value.trim() || '';
